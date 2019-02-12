43cd08efff4f0fe9ad5a37062fb0548178cbd27e
mgcioce <mgcioce@gmail.com>
2019-02-12 18:58:04 -0500

    files for test db

---
+++ sqlitedb/src/main/java/com/techtalentsouth/sqlitedb/SqlitedbApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.sqlitedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlitedbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlitedbApplication.class, args);
+    }
+
+}
+
---
+++ sqlitedb/src/main/java/com/techtalentsouth/sqlitedb/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.sqlitedb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+}
